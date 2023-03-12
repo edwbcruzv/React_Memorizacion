ee7d1fe75d0047a600f2ef2b86d20898c3017412
cruz <edwin.cruz.v@outlook.com>
2023-03-12 00:48:04 -0600

    Terminado

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,15 +1,29 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import ContadorHijo from './ContadorHijo'
+
 
 function Contador() {
     const [contador, setContador] = useState(0)
-    function sumar(params) {
+    const [input, setInput] = useState('')
+
+    const sumar= useCallback(
+      () => {
         setContador(contador + 1)
-        
-    }
-    function restar(params) {
+      },
+      [contador],
+    )
+
+    const restar=useCallback(
+      () => {
         setContador(contador - 1)
+      },
+      [contador],
+    )
+
+    function handleInput(event) {
+        setInput(event.target.value)
     }
+
 
   return (
     <div>
@@ -17,8 +31,15 @@ function Contador() {
         <nav>
             <button onClick={sumar}>+</button>
             <button onClick={restar}>-</button>
-            <ContadorHijo/>
-
+            <input type="text" onChange={handleInput} value={input}/>
+            <ContadorHijo contador={contador} sumar={sumar} restar={restar}/>
+        useCallback:
+        <br />
+        Memoriza una funcion para no volverla a definir en cada render
+        <br />
+        usarlo siempre que se pase una funcion como porp a un componente memorizado.
+        <br />
+        usarlo siempre que se pase una funcion como parametro de un efecto.
         </nav>
     </div>
   )
--- src/components/ContadorHijo.jsx
+++ src/components/ContadorHijo.jsx
@@ -1,10 +1,25 @@
-import React, { memo } from 'react'
+import React, { memo, useMemo } from 'react'
 
-function ContadorHijo() {
+
+function ContadorHijo({contador,sumar,restar}) {
+
+  const superNumero = useMemo(() =>{
+    let num=0
+    for (let i = 0; i < 1000000000;i++) {
+      num++
+    }
+    return num
+  })
+
+  
+
+
+
+
     console.log("Renderizando contador hijo")
   return (
     <div style={{border: "thin solid black", margin:"1rem",padding:"1rem"}}>
-        ContadorHijo 
+        ContadorHijo:{contador}
         <br />
         memo:Se encarga de memorizar componentes, evita renderizados inesesarios.
         <br />
@@ -17,6 +32,18 @@ function ContadorHijo() {
         un componente se vuelve muy pesado.
         <br />
         salen alertas de rendimiento en consola.
+        <br />
+        <button onClick={sumar}>+</button>
+        <button onClick={restar}>-</button>
+        <h3>{superNumero}</h3>
+
+        useMemo:
+        <br />
+        memoriza un valor calculado, es decir, el resultado de una funcion.
+        <br />
+        genera propiedades computadas.
+        <br />
+        usalo en procesos pesados.
     </div>
   )
 }
